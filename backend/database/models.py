cdafe40b25cb9cb05551b256d441866d7b2460dc
Manzi453 <i.manziyamu@alustudent.com>
2025-06-10 16:44:54 +0200

    implenation of further of the class

--- backend/database/models.py
+++ backend/database/models.py
@@ -23,6 +23,20 @@ class Transaction(Base):
         external_transaction_id (str): External vendor or partner ID.
         amount (Decimal): Transaction amount.
         t_status (str): Transaction status (e.g 'success', 'failed').
+         transaction_type (str): Type of transaction (e.g deposit, withdrawal).
+        description (str): A human readable description for the transaction.
+        date_and_time (datetime): Timestamp of the transaction.
+        transaction_fee (Decimal): Any fee charged for the transaction.
+        new_balance (Decimal): User balance after the transaction.
+        sender (str): Sender's name, number, or reference.
+        receiver (str): Receiver's name, number, or reference.
+        sender_momo_id (str): Mobile Money system ID for the sender.
+        institution_vendor (str): Vendor or institution involved, if any.
+        transaction_method (str): Method used (e.g. cash etc).
+        agent (str): Agent information for agent-based transactions.
+        withdrawer (str): Person performing the withdrawal (if applicable).
+        power_token (str): Token used for prepaid power transactions.
+    """
 
         
 
@@ -33,3 +47,4 @@ class Transaction(Base):
     external_transaction_id = Column(String(100))
     amount = Column(DECIMAL(12, 2), nullable=False)
     t_status = Column(String(20), nullable=False)
+    
