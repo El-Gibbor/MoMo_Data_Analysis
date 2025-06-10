230d85da3e0a6ba0dc834df52f53a2d401d6d35a
Manzi453 <i.manziyamu@alustudent.com>
2025-06-10 16:36:45 +0200

    implementation of function

--- backend/database/load_db.py
+++ backend/database/load_db.py
@@ -11,3 +11,29 @@ import urllib.parse
 from dotenv import load_dotenv
 
 load_dotenv()
+
+def load_to_db(session, json_path):
+    """Loads the cleaned json data to database"""
+    try:
+        with open(json_path, 'r') as f:
+            data_read = json.load(f)
+
+        transactions = []  # holds a list of all transactions (json)
+
+        for item in data_read:
+            transactions.append(Transaction(**item))
+        session.add_all(transactions)
+        session.commit()
+
+        # Clear messages for succesfull database populating
+        load_msg = f"DB LOADED: {len(transactions)} transactions from {json_path}"
+        print()
+        print('-' * len(load_msg))
+        print(load_msg)
+        print('-' * len(load_msg))
+        print()
+
+    except FileNotFoundError:
+        print(f"JSON file not found: {json_path}")
+    except json.JSONDecodeError as e:
+        print(f"JSON decode error: {e}")
